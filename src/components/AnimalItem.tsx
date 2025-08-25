80bdcc0c7957115f77613f577c47379ef4b89b9b
David Andersson <lejonmanen@gmail.com>
2025-08-25 14:24:40 +0200

    Övning 3.2c

--- src/components/AnimalItem.tsx
+++ src/components/AnimalItem.tsx
@@ -3,11 +3,13 @@ import type { Animal } from "../data/types";
 
 type Props = {
 	animal: Animal;
+	moveToTop: (species: string) => void;
 }
 
-const AnimalItem: React.FC<Props> = ({ animal }) => (
+const AnimalItem: React.FC<Props> = ({ animal, moveToTop }) => (
 	<div>
-		{animal.name} är en {animal.species}
+		{animal.name} är en {animal.species}.
+		<button onClick={() => moveToTop(animal.species)}> Flytta upp </button>
 	</div>
 )
 
--- src/components/AnimalList.tsx
+++ src/components/AnimalList.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react"
 import { animals } from "../data/animals.ts"
 import AnimalItem from "./AnimalItem.tsx"
+import type { Animal } from "../data/types.ts"
 
 const AnimalList: React.FC = () => {
+	const [data, setData] = useState<Animal[]>(animals)
+
+	const moveToTop = (species: string) => {
+		// 1. hitta objektet som ska flyttas
+		const animalToMove: Animal | undefined = data.find(a => a.species === species)
+		if( !animalToMove ) {
+			console.log('AnimalList moveToTop: hittar inte något djur att flytta')
+			return  // alternativt kan man visa ett felmeddelande för användaren
+		}
+
+		// 2. gör kopia av listan, som inte innehåller objektet som ska flyttas
+		const filteredAnimals: Animal[] = data.filter(a => a.species !== species)
+
+		// 3. gör en ny lista med flyttade objektet först - och spara i state
+		setData( [animalToMove, ...filteredAnimals] )
+	}
 
 	return (
 		<section className="framed">
-			{animals.map(a => (
-				<AnimalItem key={a.species} animal={a} />
+			{data.map(a => (
+				<AnimalItem
+					key={a.species}
+					animal={a}
+					moveToTop={moveToTop}
+					/>
 
 			))}
 		</section>
