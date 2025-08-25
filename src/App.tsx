4fea201629f3e86d29b1dd5dd4269eaf1b7f6915
David Andersson <lejonmanen@gmail.com>
2025-08-25 11:28:09 +0200

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ function App() {
 
 	return (
 		<div>
-			<h1> Don't spoil me </h1>
-			<p> Star Wars </p>
-			<Spoiler secret={'Darth Vader är Lukes pappa!'} />
+			<h1> React exempel </h1>
+			<section className="framed">
+				<h2> Don't spoil me </h2>
+				<p> Star Wars </p>
+				<Spoiler secret={'Darth Vader är Lukes pappa!'} />
 
-			<p> Harry Potter </p>
-			<Spoiler secret={'Snape kills Dumbledore'} />
+				<p> Harry Potter </p>
+				<Spoiler secret={'Snape kills Dumbledore'} />
+			</section>
 
 		</div>
 	)
